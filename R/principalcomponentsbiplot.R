20c119b08c7cd53e7a664572714ce8216136eba1
chschan <chschan@gmail.com>
2017-01-09 15:25:25 +1100

    Added normalization options for PCA biplots

--- R/principalcomponentsbiplot.R
+++ R/principalcomponentsbiplot.R
@@ -11,11 +11,13 @@
 #' x2 <- cbind(Glance=c(0.016, 0.058, 0.061, 0.038, 0.01),
 #'            FairlyThorough=c(0.022, 0.147, 0.093, 0.128, 0.022),
 #'            VeryThorough=c(0.006,0.064,0.125,0.157,0.051))
-#' rownames(x2) <- c("SomePrimary","PrimaryCompleted","SomeSecondary","SecondaryCompleted","SomeTertiary")
+#' rownames(x2) <- c("SomePrimary","PrimaryCompleted","SomeSecondary",
+#'                   "SecondaryCompleted","SomeTertiary")
 #' PrincipalComponentsBiplot(x2, output="Moonplot")
 #'
 #' @export
 PrincipalComponentsBiplot <- function(x,
+                      normalization = c("Principal", "Row principal", "Column principal", "Symmetrical", "None")[1],
                       output = c("Scatterplot", "Moonplot", "Text")[1],
                       row.names.to.remove = c("NET", "Total", "SUM"),
                       column.names.to.remove = c("NET", "Total", "SUM"))
@@ -24,10 +26,27 @@ PrincipalComponentsBiplot <- function(x,
         stop("Input should be a table with rows and columns\n")
 
     x <- GetTidyTwoDimensionalArray(x, row.names.to.remove, column.names.to.remove)
-    res <- suppressWarnings(prcomp(x))
-    pc.names <- sprintf("Component %d", 1:ncol(res$x))
-    colnames(res$x) <- pc.names
-    colnames(res$rotation) <- pc.names
+    row.names <- rownames(x)
+    col.names <- colnames(x)
+    xm <- apply(x, 2, mean)
+    z <- sweep(x, 2, xm, "-")
+    res <- svd(z)
+
+    pc.names <- sprintf("Component %d", 1:ncol(res$u))
+    colnames(res$u) <- pc.names
+    colnames(res$v) <- pc.names
+    rownames(res$u) <- row.names
+    rownames(res$v) <- col.names
+
+    res$normalization <- normalization
+    p.row <- switch(normalization, 'Principal'= 1, 'Row principal' = 1, 'Column principal' = 0,
+                                   'Symmetrical' = 0.5, 'None' = 0)
+    p.col <- switch(normalization, 'Principal'= 1, 'Row principal' = 0, 'Column principal' = 1,
+                                   'Symmetrical' = 0.5, 'None' = 0)
+
+    res$rowcoords <- sweep(res$u, 2, res$d^{p.row}, "*")
+    res$colcoords <- sweep(res$v, 2, res$d^{p.col}, "*")
+    res$output <- output
     res$output <- output
     class(res) <- "PCAbiplot"
     return(res)
@@ -41,14 +60,11 @@ PrincipalComponentsBiplot <- function(x,
 #' @export
 print.PCAbiplot <- function(x, ...)
 {
-    denom <- colSums(x$x^2)
-    pc.names <- sprintf("Component %d", 1:ncol(x$x))
     if (x$output == "Scatterplot")
     {
-       coords <- rbind(x$x[,1:2]/sqrt(denom[1]),
-                       x$rotation[,1:2])
-       groups <- rep(c("Rows","Columns"), c(nrow(x$x), nrow(x$rotation)))
-       #print(coords)
+       coords <- rbind(x$rowcoords[,1:2],
+                       x$colcoords[,1:2])
+       groups <- rep(c("Rows","Columns"), c(nrow(x$rowcoords), nrow(x$colcoords)))
        print(LabeledScatter(X = coords[, 1],
                Y = coords[, 2],
                label = rownames(coords),
@@ -64,27 +80,23 @@ print.PCAbiplot <- function(x, ...)
                x.title.font.size = 16))
     } else if (x$output == "Moonplot")
     {
-        rcoords <- x$x[,1:2]/sqrt(denom[1])
-        ccoords <- x$rotation[,1:2]
-        print(moonplot(rcoords, ccoords))
+        print(moonplot(x$rowcoords, x$colcoords))
 
     } else
     {
-        evals <- x$sdev ^ 2
+        evals <- x$d ^ 2
         pvar <- evals/sum(evals) * 100
         etab <- cbind(Eigenvalue=evals,
                       'Percent variance' = pvar,
                       'Cumulative percent' = cumsum(pvar))
-        rownames(etab) <- colnames(x$x)
+        rownames(etab) <- colnames(x$rowcoords)
 
         cat("Principal Components Analysis Biplot\n")
         cat("\nVariance explained:\n")
         print(round(etab, digits=2))
         cat("\nRow coordinates:\n")
-        # standardize columns of row.coord to have mean square 1
-        row.coord <- sweep(x$x, 2, sqrt(denom), "/")
-        print(round(row.coord, digits=2))
+        print(round(x$rowcoords, digits=2))
         cat("\nColumn coordinates:\n")
-        print(round(x$rotation, digits=2))
+        print(round(x$colcoords, digits=2))
     }
 }
