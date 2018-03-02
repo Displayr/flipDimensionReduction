9ab744e395561a11713c0af336c8f984d3448ad6
jakehoare <jake_hoare@hotmail.com>
2018-03-02 12:05:34 +1100

    DS-1841 UMAP use seed in python function

--- R/umap.R
+++ R/umap.R
@@ -11,8 +11,6 @@
 
 UMAP <- function(data, n.neighbours = 10, min.dist = 0.1, seed = 1066) {
 
-    set.seed(seed)
-
     # Convert to numeric matrix
     data <- data.matrix(data)
 
@@ -20,7 +18,9 @@ UMAP <- function(data, n.neighbours = 10, min.dist = 0.1, seed = 1066) {
 
     # Import and use python module
     umap <- import("umap")
-    output$embedding <- umap$UMAP(n_neighbors = as.integer(n.neighbours), min_dist = min.dist)$fit_transform(data)[, 1:2]
+    output$embedding <- umap$UMAP(n_neighbors = as.integer(n.neighbours),
+                                  min_dist = min.dist,
+                                  random_state = as.integer(seed))$fit_transform(data)[, 1:2]
 
     # Ensure first dimension has largest range
     if (range(output$embedding)[2] > range(output$embedding)[1])
