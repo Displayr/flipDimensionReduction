cec873b320be8aa074e4bae0243f4a5621987fac
flipDevTools <opensource@displayr.com>
2020-09-01 19:30:37 +1000

    Minor code formatting chg [revdep skip]

--- R/principalcomponentsanalysis.R
+++ R/principalcomponentsanalysis.R
@@ -783,10 +783,13 @@ convertVariableForFactorAnalysis <- function(variable, include.question.name = T
 cor.smooth2 <- function (x, eig.tol = 10^-12)
 {
     eigens <- try(eigen(x), TRUE)
-    if (inherits(eigens, as.character("try-error"))) {
-        warning("I am sorry, there is something seriously wrong with the correlation matrix,\ncor.smooth failed to  smooth it because some of the eigen values are NA.  \nAre you sure you specified the data correctly?")
-    }
-    else {
+    if (inherits(eigens, as.character("try-error")))
+    {
+        warning("I am sorry, there is something seriously wrong with the correlation matrix,",
+                "\ncor.smooth failed to  smooth it because some of the eigen values are NA.",
+                "\nAre you sure you specified the data correctly?")
+    }else
+    {
         if (min(eigens$values) < sqrt(.Machine$double.eps)) {
             warning("Matrix was not positive definite, smoothing was done")
             eigens$values[eigens$values < eig.tol] <- 100 * eig.tol
