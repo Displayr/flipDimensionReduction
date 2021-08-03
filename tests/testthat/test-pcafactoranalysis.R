bc9d7c90ba28c43178f474ad4cb81fcee0737761
Mathew W. McLean <mathew.w.mclean@gmail.com>
2021-08-03 11:06:24 +1000

    Fix expected warning in unit test [revdep skip]

--- tests/testthat/test-pcafactoranalysis.R
+++ tests/testthat/test-pcafactoranalysis.R
@@ -28,7 +28,8 @@ test_that("Smoothing of correlation matrix warnings", {
     # Set 5 missing at random
     dat <- as.data.frame(lapply(dat, function(x) { is.na(x) <- sample.int(30L, size = 5L); x}))
     expect_warning(PrincipalComponentsAnalysis(dat, missing = "Use partial data (pairwise correlations)"),
-                   "Consider using an alternative 'Missing data' option instead")
+                   paste0("Consider using an alternative ", sQuote("missing"),
+                          " argument instead"))
 })
 
 test_that("PCA: binary", {
