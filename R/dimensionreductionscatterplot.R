d005c2cd3df657041f3e612611e9eb805d6d4fc0
Justin Yap <justin.yap@displayr.com>
2024-08-14 15:09:00 +1000

    FS2-2461: Revert replace scatterplots with combined scatter (#31) (#32)
    * Revert "FS2-2461: replace scatterplots with combined scatter (#31)"
    
    This reverts commit 5fb7d80e2bb05df6a5bbb64e6150b09dc8f67c72.
    
    * Bump version and keep umap change
    
    ---------
    
    Co-authored-by: Justin Yap <JustinCCYap@users.noreply.github.com>

--- R/dimensionreductionscatterplot.R
+++ R/dimensionreductionscatterplot.R
@@ -275,6 +275,7 @@ convertFactorAnalysisTo2D <- function(x) {
 #' If created from a \code{\link{data.frame}} of variables, print as a scatterplot of grouped cases.
 #' @param x Object of class \code{"2Dreduction"}.
 #' @param ... Not used.
+#' @importFrom flipStandardCharts Chart
 #' @importFrom grDevices rgb
 #' @importFrom class knn.cv
 #' @importFrom flipU IsCount
@@ -284,25 +285,24 @@ convertFactorAnalysisTo2D <- function(x) {
 print.2Dreduction <- function(x, ...) {
 
     if (x$input.is.distance) {
-        chart <- CombinedScatter(x$embedding[, 1], x$embedding[, 2],
-                                 label = x$label,
-                                 title = x$title,
-                                 x.title = "Dimension 1",
-                                 y.title = "Dimension 2",
-                                 labels.font.size = 14,
-                                 x.title.font.size = 14,
-                                 y.title.font.size = 14,
-                                 point.radius = 4,
-                                 fixed.aspect = TRUE,
-                                 plot.border.show = TRUE,
-                                 origin = TRUE)
+        chart <- LabeledScatter(x$embedding[, 1], x$embedding[, 2],
+                       label = x$label,
+                       fixed.aspect = TRUE,
+                       title = x$title,
+                       x.title = "Dimension 1",
+                       y.title = "Dimension 2",
+                       point.radius = 4,
+                       labels.font.size = 14,
+                       x.title.font.size = 14,
+                       y.title.font.size = 14)
     }
 
     else {
         # Scatterplot with groups
         scatter.group.indices <- ""
         scatter.group.labels <- ""
-        colors <- ChartColors(12)
+        legend <- TRUE
+        colors <- "Default colors"
         title <- x$title
         set.seed(1066)
 
@@ -310,34 +310,158 @@ print.2Dreduction <- function(x, ...) {
         groups <- x$data.groups[x$used.subset]
 
         if (!is.null(groups)) {
+
             if (is.factor(groups)) {
+                scatter.group.indices <- paste(as.numeric(groups), collapse = ", ")
+                scatter.group.labels <- paste(levels(groups), collapse = ", ")
                 nearest <- knn.cv(train = embedding, cl = groups, k = 1)
                 same.category <- Sum(nearest == factor(groups, ordered = FALSE), remove.missing = FALSE) / length(groups)
                 title <- paste0(title, " - Nearest neighbor accuracy: ", sprintf("%1.2f%%", 100 * same.category))
             }
             else if (all(groups == floor(groups))) {
-                groups <- factor(groups)
-                colors <- ChartColors(length(unique(groups)), "Reds, light to dark")
+                unique.labels <- sort(unique(groups))
+                indices <- match(groups, unique.labels)
+                scatter.group.labels <- paste(unique.labels, collapse = ", ")
+                scatter.group.indices <- paste(indices, collapse = ", ")
+                colors <- "Reds, light to dark"
                 nearest <- knn.cv(train = embedding, cl = groups, k = 1)
                 same.category <- Sum(nearest == groups, remove.missing = FALSE) / length(groups)
                 title <- paste0(title, ". Nearest neighbor accuracy: ", sprintf("%1.2f%%", 100 * same.category))
             }
             else {       # numeric: create 20 buckets and treat as factors
                 groups <- cut(groups, 20)
+                scatter.group.indices <- paste(as.numeric(groups), collapse = ", ")
                 levels(groups) <- sub("[^,]*,([^]]*)\\]", "\\1", levels(groups))
-                colors <- ChartColors(20, "Reds, light to dark")
+                scatter.group.labels <- paste(levels(groups), collapse = ", ")
+                colors <- "Reds, light to dark"
             }
         }
 
-        chart <- CombinedScatter(embedding[, 1],
-                                 embedding[, 2],
-                                 group = groups,
-                                 colors = colors,
-                                 title = title,
-                                 y.title = "Dimension 2",
-                                 x.title = "Dimension 1",
-                                 labels.show = FALSE,
-                                 point.radius = 3)
+        chart <- Chart(y = embedding,
+                       type = "Scatterplot",
+                       transpose = FALSE,
+                       title = title,
+                       title.font.family = NULL,
+                       title.font.color = NULL,
+                       title.font.size = 16,
+                       colors = colors,
+                       colors.reverse = FALSE,
+                       opacity = NULL,
+                       background.fill.opacity = 0,
+                       charting.area.fill.opacity = 0,
+                       legend.fill.opacity = 0,
+                       legend.show = legend,
+                       legend.fill.color = rgb(255, 255, 255, maxColorValue = 255),
+                       legend.border.color = rgb(44, 44, 44, maxColorValue = 255),
+                       legend.border.line.width = 0,
+                       legend.font.color = NULL,
+                       legend.font.family = NULL,
+                       legend.font.size = 10,
+                       legend.position = "right",
+                       legend.ascending = TRUE,
+                       margin.top = NULL,
+                       margin.bottom = NULL,
+                       margin.left = NULL,
+                       margin.right = NULL,
+                       margin.inner.pad = NULL,
+                       y.title = "Dimension 2",
+                       y.title.font.color = NULL,
+                       y.title.font.family = NULL,
+                       y.title.font.size = 12,
+                       y.line.width = 0,
+                       y.line.color = rgb(0, 0, 0, maxColorValue = 255),
+                       y.tick.marks = "",
+                       y.tick.mark.length = 5,
+                       y.bounds.minimum = NULL,
+                       y.bounds.maximum = NULL,
+                       y.tick.distance = NULL,
+                       y.zero.line.width = 0,
+                       y.zero.line.color = rgb(44, 44, 44, maxColorValue = 255),
+                       y.position = "left",
+                       y.data.reversed = FALSE,
+                       y.grid.width = 1,
+                       y.grid.color = rgb(225, 225, 225, maxColorValue = 255),
+                       y.tick.show = TRUE,
+                       y.tick.suffix = "",
+                       y.tick.prefix = "",
+                       y.tick.decimals = NULL,
+                       y.tick.format.manual = "",
+                       y.hovertext.decimals = NULL,
+                       y.hovertext.format.manual = "",
+                       y.tick.angle = NULL,
+                       y.tick.font.color = NULL,
+                       y.tick.font.family = NULL,
+                       y.tick.font.size = 10,
+                       x.title = "Dimension 1",
+                       x.title.font.color = NULL,
+                       x.title.font.family = NULL,
+                       x.title.font.size = 12,
+                       x.line.width = 0,
+                       x.line.color = rgb(0, 0, 0, maxColorValue = 255),
+                       x.tick.marks = "",
+                       x.tick.mark.length = 5,
+                       x.bounds.minimum = NULL,
+                       x.bounds.maximum = NULL,
+                       x.tick.distance = NULL,
+                       x.zero.line.width = 0,
+                       x.zero.line.color = rgb(44, 44, 44, maxColorValue = 255),
+                       x.position = "bottom",
+                       x.data.reversed = FALSE,
+                       x.grid.width = 1,
+                       x.grid.color = rgb(225, 225, 225, maxColorValue = 255),
+                       x.tick.show = TRUE,
+                       x.tick.suffix = "",
+                       x.tick.prefix = "",
+                       x.tick.decimals = NULL,
+                       x.tick.format.manual = "",
+                       x.hovertext.decimals = NULL,
+                       x.hovertext.format.manual = "",
+                       x.tick.angle = NULL,
+                       x.tick.font.color = NULL,
+                       x.tick.font.family = NULL,
+                       x.tick.font.size = 10,
+                       x.tick.label.autoformat = TRUE,
+                       series.marker.show = "automatic",
+                       series.marker.colors = NULL,
+                       series.marker.colors.reverse = FALSE,
+                       series.marker.opacity = 1,
+                       series.marker.size = 6,
+                       series.marker.border.width = 1,
+                       series.marker.border.colors = NULL,
+                       series.marker.border.colors.reverse = FALSE,
+                       series.marker.border.opacity = 1,
+                       series.line.width = 0,
+                       series.line.colors = NULL,
+                       series.line.colors.reverse = FALSE,
+                       series.line.opacity = 1,
+                       tooltip.show = TRUE,
+                       modebar.show = FALSE,
+                       global.font.family = "Arial",
+                       global.font.color = rgb(44, 44, 44, maxColorValue=255),
+                       rows.to.ignore = "",
+                       cols.to.ignore = "",
+                       bar.gap = 0.15,
+                       data.label.show = FALSE,  # was NULL for Labeled Scatterplot
+                       data.label.font.family = NULL,
+                       data.label.font.size = 10,
+                       data.label.font.color = NULL,
+                       data.label.decimals = 0,
+                       data.label.prefix = "",
+                       data.label.suffix = "",
+                       data.label.threshold = NULL,
+                       data.label.position = "top middle",
+                       pie.order = "initial",
+                       pie.groups.order = "initial",
+                       pie.subslice.colors = NULL,
+                       pie.subslice.colors.reverse = FALSE,
+                       pie.subslice.colors.repeat = TRUE,
+                       pie.border.color = rgb(255, 255, 255, maxColorValue = 255),
+                       pie.inner.radius = NULL,
+                       pie.show.percentages = FALSE,
+                       z.title = "",
+                       scatter.group.indices = scatter.group.indices,
+                       scatter.group.labels = scatter.group.labels,
+                       us.date.format = NULL)
     }
 
     print(chart)
